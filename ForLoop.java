a4897793ecfa2b26ecd7d4dbc64f1e68f2a91a39
kusumakirankumar <kusuma.sarangapani123@gmail.com>
2022-09-29 19:25:41 +0530

    new files

--- ForLoop.java
+++ ForLoop.java
@@ -1,11 +1,8 @@
 public class ForLoop {
     public static void main(String[] args) {
         int[] a = {1,3,4,6,9,7,8,2};
-        int[] b = {1,2,3,4,5,6,7,8};
         for(int i:a) {
-            for (int j : b) {
-                System.out.println(i+j);
-            }
+            System.out.println(i);
         }
     }
 }
